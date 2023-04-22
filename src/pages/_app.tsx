37fb5573c08953eb2de356dc31108e494f4ed1e6
Patrick Lane <patricklane09@gmail.com>
2023-04-22 12:12:58 -0500

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
+import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 
 import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 );
 
 export default MyApp;
